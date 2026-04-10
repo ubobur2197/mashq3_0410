2146bc924bfc6d80bd759185e9b2a16896c4ecbd
Bobur <ubobur2197@gmail.com>
2026-04-10 15:41:02 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+# 1
+roy = [0, 1, 2, 3]
+print([x + 10 for x in roy])
+
+# 2
+roy = ["x", "y", "z"]
+print([f"harf: {x}" for x in roy])
+
+# 3
+roy = [50, 100, 150]
+print([x / 2 for x in roy])
+
+# 4
+roy = ["5", "6", "7"]
+print([int(x)**2 for x in roy])
+
+# 5
+roy = [8, 9, 10]
+print([x**2 for x in roy])
